6e339c48e8c78249827d89909388721a447736c3
Kanika Sood <kanikas@arya.cs.uoregon.edu>
2017-03-12 22:36:54 -0700

    Updated run1 files to be ex5, 6 proc, no pc

--- phase2/run1_6proc_ex5/runme.sh
+++ phase2/run1_6proc_ex5/runme.sh
@@ -1,5 +1,6 @@
 #!bin/bash
-
+#Author: Kanika Sood
+#Date: March 12, 2017
 module load mpi/mpich2-3.2_gcc-4.9
 module load tau/git
 export TAU_TRACE=1
@@ -8,4 +9,3 @@ cd /home/users/kanikas/petsc/src/ksp/ksp/examples/tutorials
 mpirun -np 6 ./ex5
 tau_treemerge.pl
 tau2slog2 tau.trc tau.edf -o tau.slog2
-
